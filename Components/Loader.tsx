380262cef9055bc952c7bf9f529218370f67f8eb
lakadlo44-cmyk <lakadlo44@gmail.com>
2026-06-29 05:24:36 +0530

    Create Loader.tsx

---
+++ Components/Loader.tsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="text-center py-10">
+      <div className="animate-pulse text-xl">
+        🤖 Gemini is generating...
+      </div>
+    </div>
+  );
+}
