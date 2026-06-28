8310f7590dc01c155064504f6559bdfb195f3cba
lakadlo44-cmyk <lakadlo44@gmail.com>
2026-06-29 05:19:46 +0530

    Create Index.ts

---
+++ lib/Index.ts
@@ -0,0 +1,21 @@
+export interface ReelIdea{
+
+title:string;
+
+hook:string;
+
+timestamp:string;
+
+text:string;
+
+caption:string;
+
+hashtags:string;
+
+audio:string;
+
+checklist:string;
+
+score:number;
+
+}
